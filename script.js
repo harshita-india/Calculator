80923b2d4941cadc00675edb11296d0dbf178877
harshita-india <harshitabaghel2005@gmail.com>
2023-05-19 12:51:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,86 @@
+var screen = document.querySelector('.input');
+let string = "";
+let buttons = document.querySelectorAll('.digit');
+Array.from(buttons).forEach((button)=>{
+  button.addEventListener('click', (e)=>{
+    if(e.target.innerHTML == '='){
+      string = eval(string);
+      document.querySelector('.input').value = string;
+    }
+   else if(e.target.innerHTML == 'C'){
+      string =  ""
+      document.querySelector('.input').value = string;
+    }
+    else{ 
+    console.log(e.target)
+    string = string + e.target.innerHTML;
+    document.querySelector('.input').value = string;
+      }
+  })
+})
+
+    function sin()
+    {
+        screen.value=Math.sin(screen.value);
+    }
+
+    function cos()
+    {
+        screen.value=Math.cos(screen.value);
+    }
+
+    function tan()
+    {
+        screen.value=Math.tan(screen.value);
+    }
+
+    function pow()
+    {
+        screen.value=Math.pow(screen.value,2);
+    }
+
+    function sqrt()
+    {
+        screen.value=Math.sqrt(screen.value,2);
+    }
+
+    function log()
+    {
+        screen.value=Math.log(screen.value);
+    }
+
+    function pi()
+    {
+        screen.value= 3.14159265359;
+    }
+
+    function e()
+    {
+        screen.value=2.71828182846;
+    }
+
+    function fact()
+    {
+        var i, num, f;
+        f=1
+        num=screen.value;
+        for(i=1; i<=num; i++)
+        {
+            f=f*i;
+        }
+
+        i=i-1;
+
+        screen.value=f;
+    }
+
+    function backspc()
+    {
+        screen.value=screen.value.substr(0,screen.value.length-1);
+    }
+   function percen()
+   {
+     var num;
+     num = screen.value;
+     screen.value= num*100;
+   }
